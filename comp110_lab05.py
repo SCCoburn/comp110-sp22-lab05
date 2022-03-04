5d9bf2e9872a38b1ecc248af136ea2896f559beb
Brian Coburn <bcoburn@sandiego.edu>
2022-03-03 20:35:35 -0800

    finished lab

--- comp110_lab05.py
+++ comp110_lab05.py
@@ -4,6 +4,9 @@ Module: comp110_lab05
 Exercises from lab 05, dealing with strings and file reading.
 """
 
+"from fcntl import F_GETLEASE"
+
+
 def max_wind_speed(hurricane_filename):
     """
     Finds the maximum wind speed for the hurricane.
@@ -14,9 +17,17 @@ def max_wind_speed(hurricane_filename):
     Returns:
     (type: int) The maximum wind speed of the hurricane.
     """
+    max_speed = 0
+    my_file = open(hurricane_filename, 'r')
+    for line in my_file:
+        vals = line.split(',')
+        if int(vals[4]) > max_speed:
+            max_speed = int(vals[4])
+    my_file.close()
+    return max_speed
+    
 
-    pass # replace this line with your implementation of this function
-
+        
 
 def contains_word(word, review):
     """
@@ -41,13 +52,29 @@ def test_max_wind_speed():
     """ Function that tests the max_wind_speed function. """
 
     print("Starting test of max_wind_speed")
+    max_wind_speed("dorian.csv")
+    if max_wind_speed ("dorian.csv") == 185 :
+        print("max_wind_speed(dorian.csv) PASSED")
+    else:
+        print("max_wind_speed(dorian.csv) FAILED")
+    max_wind_speed("florence.csv")
+    if max_wind_speed("florence.csv") == 140 :
+        print("max_wind_speed(florence.csv) PASSED")
+    else:
+        print("max_wind_speed(florence.csv) FAILED")
+    max_wind_speed("irma.csv")
+    if max_wind_speed("irma.csv") == 185:
+        print("max_wind_speed(irma.csv) PASSED")
+    else:
+        print("max_wind_speed(irma.csv) FAILED")
+    
 
     # To Do: Call your max_wind_speed function on the irma.csv file, using an if
     # statement to indicate whether the result was correct or not.
     # Then repeat the process for the florence.csv and dorian.csv files to check
     # whether your function works for those files.
 
-    print("FAILED: Not implemented yet.") # remove this line when you finish the to do
+   
 
     print("Done testing max_wind_speed")
 
